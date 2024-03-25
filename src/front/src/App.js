228631085d8a17b8c3cc2fc021c60c7d03d0ebc0
이호철 <9418261@ict-companion.com>
2024-03-25 14:50:20 +0900

    모달, 다이얼로그 테스트 작성

--- src/front/src/App.js
+++ src/front/src/App.js
@@ -1,13 +1,25 @@
 import * as React from 'react';
-import PersistentDrawerLeft from "./ui/PersistentDrawerLeft";
-//import Dirs from "./components/DirInfo";
-import SettingStock from "./components/setting";
 
+import AlertDialog from "./ui/AlertDialog";
+import ModalUnstyled from "./ui/Register";
 function App() {
-  return (
-      <div>
-        <SettingStock />
-      </div>
-  );
+
+    //서버에서 데이터를 받아 온다.
+    //데이터 확인에따라 세팅이 안되어 있다는 alert를 띄워준다.
+
+    function AlertFunc(){
+        alert("잘 전달 되나??");
+    }
+
+    return (
+        <div>
+            <AlertDialog title={"알림"} 
+                         msg={"한국투자 증권 설정 파일이 존재 하지 않습니다."}
+                         complateButton={"설정화면"}
+                         closeButton={"닫기"}
+                         AlertFunc={AlertFunc}/>
+            <ModalUnstyled />
+        </div>
+    );
 }
 export default App;
